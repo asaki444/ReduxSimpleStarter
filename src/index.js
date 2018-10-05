8e1f95606cdd336351d785133f522c1160c5f36c
Aditi Green <aditigreen@aditis-mbp.home>
2018-10-05 00:36:34 -0400

    Added back src folder

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// Create a new component, should produce some HTML
+// some HTML
+
+//  Take this component generated HTML and put it on the page(in the DOM)
+
+const App = function (){
+    return <div>Hi!</div>
+}
+
+ReactDOM.render(App)
